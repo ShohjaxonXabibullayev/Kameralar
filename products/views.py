ca82462824ab6b19805ef0e031e438e907221e61
ShohjaxonXabibullayev.Dev <shohjaxonxabibullayev90@gmail.com>
2025-07-05 14:02:36 +0500

    Update views.py

--- products/views.py
+++ products/views.py
@@ -4,29 +4,27 @@ from .models import Cameralar
 from .forms import ProductForm
 
 
-# Mahsulotlar ro'yxati
 class CameraListView(ListView):
     model = Cameralar
-    template_name = 'products_list.html'  # qaysi HTML faylga chiqadi
-    context_object_name = 'products'  # {{ products }} orqali foydalaniladi
+    template_name = 'products_list.html'  
+    context_object_name = 'products'  
 
 
-# Bitta mahsulotning tafsiloti
 class CameraDetailView(DetailView):
     model = Cameralar
     template_name = 'product_detail.html'
     context_object_name = 'product'
 
 
-# Mahsulot qo'shish
+
 class CameraCreateView(CreateView):
     model = Cameralar
-    form_class = ProductForm  # forms.py dan formani olib ishlatadi
+    form_class = ProductForm  
     template_name = 'product_update.html'
-    success_url = reverse_lazy('products_list')  # muvaffaqiyatli qo‘shilgandan keyin qayerga yo‘naltirish
+    success_url = reverse_lazy('products_list') 
 
 
-# Mahsulotni tahrirlash
+
 class CameraUpdateView(UpdateView):
     model = Cameralar
     form_class = ProductForm
@@ -34,7 +32,7 @@ class CameraUpdateView(UpdateView):
     success_url = reverse_lazy('product_list')
     context_object_name = 'product'
 
-# Mahsulotni o‘chirish
+
 class CameraDeleteView(DeleteView):
     model = Cameralar
     template_name = 'product_delete.html'
